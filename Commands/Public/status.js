0c86c0c3578168c6017555c73f187f77365d2e1a
Cxetive <54766299+Cxetive@users.noreply.github.com>
2022-11-29 13:30:03 +0100

    Update status.js

--- Commands/Public/status.js
+++ Commands/Public/status.js
@@ -1,25 +1,32 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+const { EmbedBuilder } = require('discord.js')
 const fivem = require("discord-fivem-api");
 const server = new fivem.DiscordFivemApi("45.140.191.13:25565");
 
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("status")
-    .setDescription("ja"),
+    .setDescription("Will repond with pong."),
     async execute(client, interaction) {
         const { guild, member, message } = interaction;
-        const status = server.getServerStatus()
 
-        console.log(status)
-        const AantalSpelers = await server.getPlayersOnline()
-        const embed = new EmbedBuilder()
-        .addFields(
-            { name: `**Server Status**`, value: `🟢 Online`},
-            { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``},
-            { name: `**Aantal Spelers:**`, value: `\`\`\`${AantalSpelers}\`\`\``}
-        )
-        
+        server.getPlayers().then((data) => {
+            const AantalSpelers = await server.getPlayersOnline()
+            const embed = new EmbedBuilder()
+                .addFields(
+                    { name: `**Server Status**`, value: `🟢 Online`},
+                    { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``},
+                    { name: `**Aantal Spelers:**`, value: `\`\`\`${AantalSpelers}\`\`\``}
+                )
+
+                interaction.reply({embeds: [embed]});
+        }).catch((err) => {
+            const embed = new EmbedBuilder()
+                .addFields(
+                    { name: `**Server Status**`, value: `🔴 Offline`},
+                )
+                interaction.reply({embeds: [embed]})
+        });
+
     }
 
-    
 }
