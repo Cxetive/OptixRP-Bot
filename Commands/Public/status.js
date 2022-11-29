793f583de89891bb35b27e0ca3bb3ad118309220
Cxetive <54766299+Cxetive@users.noreply.github.com>
2022-11-29 13:42:14 +0100

    Update status.js

--- Commands/Public/status.js
+++ Commands/Public/status.js
@@ -1,36 +1,36 @@
-const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
-const fivem = require("discord-fivem-api");
-const server = new fivem.DiscordFivemApi("45.140.191.13:25565");
+    const { EmbedBuilder, SlashCommandBuilder } = require('discord.js')
+    const fivem = require("discord-fivem-api");
+    const server = new fivem.DiscordFivemApi("45.140.191.13:25565");
 
-module.exports = {
-    data: new SlashCommandBuilder()
-    .setName("status")
-    .setDescription("Will repond with pong."),
-    async execute(interaction) {
-        const { guild, member, message } = interaction;
+    module.exports = {
+        data: new SlashCommandBuilder()
+        .setName("status")
+        .setDescription("Will repond with pong."),
+        async execute(interaction) {
+            const { guild, member, message } = interaction;
 
-        const status = server.getServerStatus()
-        console.log(`Status : ${server.getPlayers()}`)
-        if(status === "online") {
-            const AantalSpelers = await server.getPlayersOnline()
-            const embed = new EmbedBuilder()
-                .addFields(
-                    { name: `**Server Status**`, value: `🟢 Online`},
-                    { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``},
-                    { name: `**Aantal Spelers:**`, value: `\`\`\`${AantalSpelers}\`\`\``}
-                )
+            const status = server.getServerStatus()
+            console.log(`Status : ${server.getPlayers()}`)
+            if(server.getPlayers()) {
+                const AantalSpelers = await server.getPlayersOnline()
+                const embed = new EmbedBuilder()
+                    .addFields(
+                        { name: `**Server Status**`, value: `🟢 Online`},
+                        { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``},
+                        { name: `**Aantal Spelers:**`, value: `\`\`\`${AantalSpelers}\`\`\``}
+                    )
 
-                interaction.reply({embeds: [embed]});
+                    interaction.reply({embeds: [embed]});
 
-        } else { 
-            const embed = new EmbedBuilder()
-                .addFields(
-                    { name: `**Server Status**`, value: `🔴 Offline`},
-                )
+            } else { 
+                const embed = new EmbedBuilder()
+                    .addFields(
+                        { name: `**Server Status**`, value: `🔴 Offline`},
+                    )
 
-                interaction.reply({embeds: [embed]});
+                    interaction.reply({embeds: [embed]});
+            }
+
         }
-
+        
     }
-    
-}
