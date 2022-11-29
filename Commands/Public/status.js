6d7481429577ed2937e751ae4ab1cd6865ec70f3
Cxetive <54766299+Cxetive@users.noreply.github.com>
2022-11-29 15:14:50 +0100

    Update status.js

--- Commands/Public/status.js
+++ Commands/Public/status.js
@@ -8,36 +8,37 @@
         .setDescription("Will repond with pong."),
         async execute(interaction) {
             const { guild, member, message } = interaction;
+            var interval = setInterval (function () {
             server.getPlayers().then((data) => {
                 let result  = [];
                 let index = 1;
                 for (let player of data) {
                   result.push(`${index++}. ${player.name} | ${player.id} ID | ${player.ping} ping\n`);
                 }
-
+                const status = server.getServerStatus()
+                console.log(`Status : ${server.getPlayers()}`)
+                if(server.getPlayers()) {
+                    const embed = new EmbedBuilder()
+                        .addFields(
+                            { name: `**Server Status**`, value: `\`\`\`Online\`\`\``},
+                            { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``, inline: true},
+                            { name: `**Aantal Spelers:**`, value: `\`\`\`${data.length}\`\`\``, inline: true}
+                        )
+    
+                        interaction.reply({embeds: [embed]});
+    
+                } else { 
+                    const embed = new EmbedBuilder()
+                        .addFields(
+                            { name: `**Server Status**`, value: `\`\`\`Offline\`\`\``},
+                        )
+    
+                        interaction.reply({embeds: [embed]});
+                }
                 console.log(result)
             })
-            const status = server.getServerStatus()
-            console.log(`Status : ${server.getPlayers()}`)
-            if(server.getPlayers()) {
-                const AantalSpelers = await server.getPlayersOnline()
-                const embed = new EmbedBuilder()
-                    .addFields(
-                        { name: `**Server Status**`, value: `🟢 Online`},
-                        { name: `**Connect**`, value: `\`\`\`F8 -> connect 45.140.191.13:25565\`\`\``},
-                        { name: `**Aantal Spelers:**`, value: `\`\`\`${AantalSpelers}\`\`\``}
-                    )
-
-                    interaction.reply({embeds: [embed]});
-
-            } else { 
-                const embed = new EmbedBuilder()
-                    .addFields(
-                        { name: `**Server Status**`, value: `🔴 Offline`},
-                    )
-
-                    interaction.reply({embeds: [embed]});
-            }
+           
+        }, 1 * 1000);
 
         }
         
