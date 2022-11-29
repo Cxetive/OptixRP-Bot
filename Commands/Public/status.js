04edd5c7c3e90f88e07154c811f24407f394b3aa
Cxetive <54766299+Cxetive@users.noreply.github.com>
2022-11-29 13:38:47 +0100

    Update status.js

--- Commands/Public/status.js
+++ Commands/Public/status.js
@@ -10,7 +10,7 @@ module.exports = {
         const { guild, member, message } = interaction;
 
         const status = server.getServerStatus()
-
+        console.log(`Status : ${status}`)
         if(status === "online") {
             const AantalSpelers = await server.getPlayersOnline()
             const embed = new EmbedBuilder()
