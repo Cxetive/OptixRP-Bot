052200c3a9b1017a7371ab31dbcc19676ff9d9ac
Cxetive <devanoweber@gmail.com>
2022-12-02 11:47:22 +0100

    Clock uit

---
+++ Commands/Moderation/clock/uit.js
@@ -0,0 +1,31 @@
+const { SlashCommandBuilder, EmbedBuilder } = require('discord.js')
+
+
+module.exports = {
+    subCommand: "clock.uit",
+
+    /**
+     * @param {ChatInputCommandInteraction} interaction;
+     * @param {Client} client;
+     */
+
+    async execute(client, interaction) {
+
+        const { guild, member } = interaction;
+
+        const embed = new EmbedBuilder()
+            .setDescription(`${member} Heeft zich zojuist uitgelockt en is niet meer in dienst als Staff`)
+            .setTimestamp()
+            .setColor("#49fc03")
+
+
+            new WebhookClient({ 
+                url: 'https://discord.com/api/webhooks/1048188203867308053/8YjB33n1sybzJjFF8oN0wk-yhQtrAP6YPHu8SoaxPY80gEgd3M5DPnr25heoauKzR4vc'
+             })
+             .send({ embeds: [embed] })
+             .catch((err) => {
+               console.log(err);
+             });;
+
+    }
+}
